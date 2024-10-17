7159ce4607bdcb1889d5f74508e9aa3235e90849
codingdsb <codingdsb@gmail.com>
2024-10-17 20:04:41 +0530

    improved bubble sort

--- sorting_algorithms/python/bubble_sort.py
+++ sorting_algorithms/python/bubble_sort.py
@@ -1,7 +1,7 @@
 def bubble_sort(numbers):
     for n in range(len(numbers)-1):
         swaps = 0
-        for i in range(len(numbers)-1):
+        for i in range(len(numbers)-n-1):
             if numbers[i] > numbers[i+1]:
                 swaps += 1
                 numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
